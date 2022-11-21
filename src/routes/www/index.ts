43bb5a6a1f8919618d908f655629dd0fe1bb84b5
skdud4659 <skdud4659@gmail.com>
2022-11-21 19:35:28 +0900

    [feat] - global :: font

--- src/routes/www/index.ts
+++ src/routes/www/index.ts
@@ -2,13 +2,11 @@ import { Application } from 'express'
 
 import main from './pages/main'
 import posts from './pages/posts'
-import contact from './pages/contact'
 
 class WWWRouter {
   public routes(basePath: string, app: Application): void {
     app.use(basePath, main)
     app.use(basePath, posts)
-    app.use(basePath, contact)
 
   }
 }
--- src/routes/www/pages/contact.ts
+++
@@ -1,9 +0,0 @@
-import {Router, Request, Response} from 'express'
-
-const router = Router()
-
-router.get('/contact', (req: Request, res: Response) => {
-  res.render('www/contact')
-})
-
-export default router
