e6a6f72b89dcd7482ce5ca43d233358c2431fe52
skdud4659 <skdud4659@gmail.com>
2023-01-09 21:29:26 +0900

    [feat] - basic javascript

---
+++ publishing/dom/assets/js/script.js
@@ -0,0 +1,64 @@
+// const app = {
+//   lang: 'ko',
+//   value: 100,
+//   getNumber() {
+//     return this.value;
+//   },
+//   updateNumber(value) {
+//     this.value = value;
+//   }
+// }
+//
+// let number = app.getNumber();
+// console.log(number);
+//
+// app.updateNumber(1000);
+// number = app.getNumber();
+// console.log(number);
+
+const APP = {
+  _isOpen: false,
+  init() {
+    this.layout()
+    this.addEvent()
+    this.reset()
+  },
+  layout() {
+    this.navEl = document.querySelector('#global-navigation')
+    this.biEl = document.querySelector('#bi')
+  },
+  addEvent() {
+    this.biEl.addEventListener('click', this.handleClickBiEl)
+    this.navEl.addEventListener('click', this.handleClickNavEl.bind(this))
+    window.addEventListener('resize', this.handleResizeWindow.bind(this))
+    window.addEventListener('scroll', this.handleScrollWindow.bind(this))
+  },
+  removeEvent() {
+    this.biEl.removeEventListener('click')
+    window.removeEventListener('resize')
+    window.removeEventListener('scroll')
+  },
+  reset() {
+    this._isOpen = false
+  },
+  handleClickNavEl() {
+    console.log('navigation')
+  },
+  handleClickBiEl(e) {
+    e.preventDefault() // 요소의 기본 이벤트 발생되지 않도록 방지
+    e.stopPropagation()  // 부모요소의 이벤트 버블링 방지
+    // bind(this)로 this를 받고 있을 경우 > APP 자체를 바인딩 받음
+    // bind(this)가 없을 경우 this > event가 발생하는 주체
+    console.log('click')
+  },
+  handleResizeWindow() {
+    const { innerHeight: height, innerWidth: width } = window
+    console.log('resize!!', { width, height })
+  },
+  handleScrollWindow() {
+    const { scrollY: posY } = window
+    console.log('scroll!!', posY)
+  }
+}
+
+APP.init()
